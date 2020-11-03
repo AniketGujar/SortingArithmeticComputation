f639a53f1f78e89cbca6e72a314d8bfa7ec9efad
AniketGujar <gujaraniket7875@gmail.com>
2020-11-03 17:07:34 +0530

    Added: First Computation

--- ArimtCompNSorting.sh
+++ ArimtCompNSorting.sh
@@ -1,3 +1,9 @@
+#!/bin/bash -x
+
+echo "This problem computes different arithmetic expressions and Sorts the results"
+
 read -p "Enter the value a  =" a
 read -p "Enter the value b = " b
 read -p "Enter the value c = " c
+
+c1=`expr $a + $b \* $c`
