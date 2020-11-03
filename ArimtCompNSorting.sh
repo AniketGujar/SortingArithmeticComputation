1b68d0fcdffa3b540f5ec59b36609a12853097b7
AniketGujar <gujaraniket7875@gmail.com>
2020-11-03 16:32:46 +0530

    Added:Updted ArimtCompNSorting.sh Displaying Problem Statement

--- ArimtCompNSorting.sh
+++ ArimtCompNSorting.sh
@@ -1,3 +1,3 @@
-read -p "Enter the value a  =" a
-read -p "Enter the value b = " b
-read -p "Enter the value c = " c
+#!/bin/bash
+
+echo "This problem computes different arithmetic expressions and Sorts the results"
