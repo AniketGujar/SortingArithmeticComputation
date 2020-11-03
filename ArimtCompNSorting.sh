1a8b0a66bb5053e78deb51e7bfd0764a03af551b
AniketGujar <gujaraniket7875@gmail.com>
2020-11-03 16:08:37 +0530

    Added: Code to take 3 inputs from the user

--- ArimtCompNSorting.sh
+++ ArimtCompNSorting.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-
-echo "This problem computes different arithmetic expressions and Sorts the results"
+read -p "Enter the value a  =" a
+read -p "Enter the value b = " b
+read -p "Enter the value c = " c
